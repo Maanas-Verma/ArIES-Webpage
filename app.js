a28a54534401ba6ec373c5d94e2e65c9e26306a9
Avichal Gupta <avichalee@gmail.com>
2022-01-12 23:27:20 +0530

    changed port

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.listen(process.env.PORT || 8081, function(){
    console.log("The YelpCamp Server Has Started!");
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 5000, function () {
     var host = server.address().address
     var port = server.address().port
  
